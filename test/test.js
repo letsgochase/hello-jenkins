1f44e9b334afb4801558206ddac9c84e5ad6fd55
Nam Dinh <thunderbird0304@gmail.com>
2017-09-06 17:55:10 +0700

    test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins6', done);
+    request(app).get('/').expect('hello jenkins7', done);
   });
 });
