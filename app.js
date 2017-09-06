0c847248b4a86b54f9949d252f2fd7927e967878
Nam Dinh <thunderbird0304@gmail.com>
2017-09-06 17:48:29 +0700

    8

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins4');
+  res.send('hello jenkins5');
 });
  
 	
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins4', done);
+    request(app).get('/').expect('hello jenkins5', done);
   });
 });
